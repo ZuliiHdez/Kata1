b84d25e5928f21ab60bfbe90fb2e3456166997b3
Zuleima <zuleima2304@gmail.com>
2024-01-19 12:43:04 +0000

    Segunda Versión.

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Person {
+package es.ulpgc.kata1;
+
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+    private String nombre;
+    private LocalDate cumpleaños;
+    private long MILLISECONDS_PER_YEAR = (long) (100*60*60*24*365.25);
+
+    public Person(String nombre, LocalDate cumpleaños) {
+        this.nombre = nombre;
+        this.cumpleaños = cumpleaños;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getCumpleaños() {
+        return cumpleaños;
+    }
+    public int getEdad(){
+        return (int) (Period.between(cumpleaños,LocalDate.now()).getYears());
+    }
+
+    public long milliSecondsToYear(long milles) {
+        return milles/MILLISECONDS_PER_YEAR;
+    }
 }
